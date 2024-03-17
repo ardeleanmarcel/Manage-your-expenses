71f540a73ee78a87e7b2f26f5a33b0d45ba5503e
Valentin Harapu <alexandru.harapu@gmail.com>
2024-03-17 14:36:15 +0200

    Switch from CRA to Vite

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Manage your expenses link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Manage your expenses/i);
-  expect(linkElement).toBeInTheDocument();
-});
