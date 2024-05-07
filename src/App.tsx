4110377ba95d7d29e0bc194ad9ede700a9d000f6
Marcel <marcel.ardelean@vspartners.com>
2024-05-07 13:14:38 +0300

    update styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { ThemeProvider, createTheme } from '@mui/material'
 
 import Register from './Pages/Register/Register'
-import './App.css'
 import Home from './Pages/Home/Home'
+import './App.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4B0082',
+    },
+  },
+})
 
 export default function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="register" element={<Register />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   )
 }
